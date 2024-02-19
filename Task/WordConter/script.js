8a018adb361c6399322a582a9d0ee49b9b254c78
Shreyansh2912 <shreyanshshah2912@gmail.com>
2024-02-19 20:34:32 +0530

    Data

---
+++ Task/WordConter/script.js
@@ -0,0 +1,32 @@
+// let a = document.querySelector("text");
+// let a = document.getElementById("text");
+// console.log(a);
+
+function data() {
+    let a = document.getElementById('text').value;
+    
+    let words = a.split(" ").length
+    document.getElementById("words").innerHTML = words
+
+    let length = a.length;
+    document.getElementById("char").innerHTML = length
+
+    let char_space = a.split(" ").join("").length;
+    document.getElementById("char_space").innerHTML = char_space;
+
+    let sentences = a.split(". ").length;
+    document.getElementById("sentence").innerHTML = sentences
+
+    let reading = words/3
+    let min = (words/4)/59;
+    reading = reading%60
+    if(parseInt(reading) == 59){
+        reading = 0; 
+    }
+    // console.log(`${parseInt(min)} : ${parseInt(reading)}`);
+    let Reading_time = `${parseInt(min)} : ${parseInt(reading)}`;
+    document.getElementById("read").innerHTML = Reading_time;
+
+
+    let speaking = words/2.5
+}
